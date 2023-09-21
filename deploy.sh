f0a5b9f483ee1548a037443dedaebc6b2eaffec0
Kareem Etaam <kareem.ataam12345@gmail.com>
2023-09-21 21:17:15 +0000

    Document the deploy file

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 IMAGE_NAME=kareemetaam/java-app:$1
 
+#Pull the image from docker hub
 docker pull $IMAGE_NAME
 
+#Run a container of the pulled image
 docker run -d --name java_app_$1 $IMAGE_NAME
